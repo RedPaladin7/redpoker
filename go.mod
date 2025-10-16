f0a7fe1ea6a84a3fc4c8042401ea8432faf00924
RedPaladin7 <ishans7325@gmail.com>
2025-10-17 00:14:23 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RedPaldin7/redpoker
+
+go 1.25.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/RedPaldin7/redpoker/deck"
+)
+
+func main(){
+
+	d := deck.New()
+	fmt.Println(d)
+}
